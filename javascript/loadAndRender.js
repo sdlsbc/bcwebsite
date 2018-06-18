f4705e94ca734c7bde1da3633c48538818dad263
Ankita Mhatre <am@broadwayconnected.com>
2018-06-18 11:37:25 -0400

    fetching fresh data for modal

--- javascript/loadAndRender.js
+++ javascript/loadAndRender.js
@@ -38,10 +38,10 @@ function loadAndShowPosts() {
 
 function getPostsItems() {
 	let url = "";
-	if(PAGE == 'new'){
+	if (PAGE == 'new') {
 		url = "http://app.bwayconnected.com/api/posts?offset=" + (12 * fetchCount) + "&limit=12";
 	}
-	if(PAGE == 'fav'){
+	if (PAGE == 'fav') {
 		let user_id = localStorage.getItem("user_id");
 		url = "http://app.bwayconnected.com/api/post/favourites?user_id=" + user_id + "&offset=" + (12 * fetchCount) + "&limit=12";
 	}
@@ -97,108 +97,136 @@ function favorite(post_id) {
 }
 
 
-
 function showModal(body) {
 	let user_id = localStorage.getItem("user_id");
-	var img_address = body.post_image;
-	var post_image = document.getElementsByClassName('modal-image');
-	post_image[0].innerHTML = '<img src=' + img_address + '>';
 
-	var profile_image_address = body.publisher.profile_image;
+	//fetch fresh data from api to display in Modal
 
-	// check if profile picture is default
-	if (profile_image_address == "http://app.bwayconnected.com/public/images/default.jpg") {
-		profile_image_address = "http://app.bwayconnected.com/public/images/T3uVwB96tW07.png"
-	}
+	var url = 'http://app.bwayconnected.com/api/post/detail?post_id=' + body.id;
 
-	var profile_image = document.getElementsByClassName("modal-header");
-	profile_image[0].innerHTML = '<img src=' + profile_image_address + '>';
+	fetch(url, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	})
+		.then(res => res.json())
+		.catch(error => console.error('Error:', error))
+		.then(response => {
 
-	var publisher_name = document.getElementsByClassName("modal_h3");
-	publisher_name[0].innerHTML = body.publisher.first_name + ' ' + body.publisher.last_name;
-
-
-  var dateTime = body.published_date;
-	var dateTime = dateTime.split(" ");
-	var date1 = dateTime[0];
-	var time1 = dateTime[1];
-
-	var real_date = Date(date1);
-	var real_date = real_date.split(" ");
-	var real_date0 = real_date[0];
-	var real_date1 = real_date[1];
-	var real_date2 = real_date[2];
-	var really_real_date = [real_date0] + " " + [real_date1] + " " + [real_date2];
-
-	var date = document.getElementsByClassName("modal-date");
-	date[0].innerHTML = really_real_date;
- //  var time = document.getElementsByClassName("modal-time");
-	// time[0].innerHTML = time1;
-   
-  var time_split = time1.split(':');
-  var hours = Number(time_split[0]);
-  var minutes = Number(time_split[1]);
-  var seconds = Number(time_split[2]);
-  
-  var timeValue;
-
-  if (hours > 0 && hours <= 12) {
-    timeValue= "" + hours;
-	} else if (hours > 12) {
-  	timeValue= "" + (hours - 12);
-	} else if (hours == 0) {
-  	timeValue= "12";
-	}
-  
-  timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
-	// timeValue += (seconds < 10) ? ":0" + seconds : ":" + seconds;  // get seconds
-	timeValue += (hours >= 12) ? " P.M." : " A.M.";  // get AM/PM
-
-  var timexxx = document.getElementsByClassName("modal-time");
-	timexxx[0].innerHTML = timeValue;
+			// check if request successful
+			if (response.Response == "1000") {
+				console.log("request not successful");
+				// createCustomAlert("WARNING: User Already Exists");
+			} else {
+				console.log('Success:', response)
+				var body = response.Result;
+				// console.log('data fetched for modal', body);
+				console.log('posts: ', body.Posts[0].id);
 
 
+				var img_address = body.Posts[0].post_image;
+				var post_image = document.getElementsByClassName('modal-image');
+				post_image[0].innerHTML = '<img src=' + img_address + '>';
 
+				var profile_image_address = body.Posts[0].publisher.profile_image;
 
-	var title = document.getElementsByClassName("modal-title");
-	title[0].innerHTML = body.title;
-
-	var description = document.getElementsByClassName("modal-description");
-	description[0].innerHTML = body.description;
-
-	var url = body.source_url;
-	var url_x = document.getElementById('modal-other');
-	url_x.href = url;
-
-	console.log("url");
-	console.log(url);
-	if (url == "") {
-		document.getElementsByClassName("read_more")[0].style.display = "none";
-	} else {
-		document.getElementsByClassName('read_more')[0].style.display = "block";
-		var url_x = document.getElementById('modal-other');
-		url_x.href = url;
-	}
-
-	modal.style.display = "block";
- 
-	let fav_button = document.getElementById('modal-favorite-img')
-
-	if (body.favourites.some(fav => fav.user_id == user_id)) {
-		fav_button.classList.add('modal-favorite-clicked');
-	}
-
-	fav_button.onclick = function (ev) {
-		favorite(body.id)
-			.then(body => {
-				if (body.Message === "Added to user favourite successfully") {
-					ev.srcElement.classList.add('modal-favorite-clicked');
-				} else {
-					ev.srcElement.classList.remove('modal-favorite-clicked');
+				// check if profile picture is default
+				if (profile_image_address == "http://app.bwayconnected.com/public/images/default.jpg") {
+					profile_image_address = "http://app.bwayconnected.com/public/images/T3uVwB96tW07.png"
 				}
-			})
-	};
 
+				var profile_image = document.getElementsByClassName("modal-header");
+				profile_image[0].innerHTML = '<img src=' + profile_image_address + '>';
+
+				var publisher_name = document.getElementsByClassName("modal_h3");
+				publisher_name[0].innerHTML = body.Posts[0].publisher.first_name + ' ' + body.Posts[0].publisher.last_name;
+
+
+				var dateTime = body.Posts[0].published_date;
+				var dateTime = dateTime.split(" ");
+				var date1 = dateTime[0];
+				var time1 = dateTime[1];
+
+				var real_date = Date(date1);
+				var real_date = real_date.split(" ");
+				var real_date0 = real_date[0];
+				var real_date1 = real_date[1];
+				var real_date2 = real_date[2];
+				var really_real_date = [real_date0] + " " + [real_date1] + " " + [real_date2];
+
+				var date = document.getElementsByClassName("modal-date");
+				date[0].innerHTML = really_real_date;
+				//  var time = document.getElementsByClassName("modal-time");
+				// time[0].innerHTML = time1;
+
+				var time_split = time1.split(':');
+				var hours = Number(time_split[0]);
+				var minutes = Number(time_split[1]);
+				var seconds = Number(time_split[2]);
+
+				var timeValue;
+
+				if (hours > 0 && hours <= 12) {
+					timeValue = "" + hours;
+				} else if (hours > 12) {
+					timeValue = "" + (hours - 12);
+				} else if (hours == 0) {
+					timeValue = "12";
+				}
+
+				timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
+				// timeValue += (seconds < 10) ? ":0" + seconds : ":" + seconds;  // get seconds
+				timeValue += (hours >= 12) ? " P.M." : " A.M.";  // get AM/PM
+
+				var timexxx = document.getElementsByClassName("modal-time");
+				timexxx[0].innerHTML = timeValue;
+
+				var title = document.getElementsByClassName("modal-title");
+				title[0].innerHTML = body.Posts[0].title;
+
+				var description = document.getElementsByClassName("modal-description");
+				description[0].innerHTML = body.Posts[0].description;
+
+				var url = body.Posts[0].source_url;
+				var url_x = document.getElementById('modal-other');
+				url_x.href = url;
+
+				console.log("url");
+				console.log(url);
+				if (url == "") {
+					document.getElementsByClassName("read_more")[0].style.display = "none";
+				} else {
+					document.getElementsByClassName('read_more')[0].style.display = "block";
+					var url_x = document.getElementById('modal-other');
+					url_x.href = url;
+				}
+
+				modal.style.display = "block";
+
+				let fav_button = document.getElementById('modal-favorite-img')
+
+				if (body.Posts[0].favourites.some(fav => fav.user_id == user_id)) {
+					fav_button.classList.add('modal-favorite-clicked');
+				}
+
+				fav_button.onclick = function (ev) {
+					favorite(body.Posts[0].id)
+						.then(body => {
+							if (body.Posts[0].Message === "Added to user favourite successfully") {
+								ev.srcElement.classList.add('modal-favorite-clicked');
+							} else {
+								ev.srcElement.classList.remove('modal-favorite-clicked');
+							}
+						})
+				};
+
+
+
+			}
+		})
+
+	//fetch data end
 }
 
 function createPost(body) {
@@ -281,13 +309,13 @@ function createPost(body) {
 	button_div.appendChild(fav_button);
 
 
-  let fav_button_num = document.createElement('p');
-  let fav_button_numNode = document.createTextNode(body.likes);
-  fav_button_num.appendChild(fav_button_numNode);
-  fav_button_num.classList.add('fav_likes');
-  button_div.appendChild(fav_button_num);
+	let fav_button_num = document.createElement('p');
+	let fav_button_numNode = document.createTextNode(body.likes);
+	fav_button_num.appendChild(fav_button_numNode);
+	fav_button_num.classList.add('fav_likes');
+	button_div.appendChild(fav_button_num);
 
-  
+
 	let share_button = document.createElement('img');
 	share_button.src = '../images/newsfeed_buttons/share.png';
 	share_button.classList.add('button');
@@ -302,27 +330,27 @@ function createPost(body) {
 	comment_button.src = '../images/newsfeed_buttons/comment.png';
 	comment_button.classList.add('comment_button');
 	button_div.appendChild(comment_button);
-  
-  let comment_message = document.createElement('div')
-  // comment_message.classList.add('comment_message');
-  button_div.appendChild(comment_message);
-  comment_message.classList.add('hidden')
 
-  comment_button.onclick = function (ev) {
+	let comment_message = document.createElement('div')
+	// comment_message.classList.add('comment_message');
+	button_div.appendChild(comment_message);
+	comment_message.classList.add('hidden')
+
+	comment_button.onclick = function (ev) {
 		// let comment_buttonx = document.getElementsByClassName("comment_message")[0];
-	 //  comment_buttonx.display = "block";
-  //   comment_buttonx.classList.remove('hidden');
-    createCustomAlert("Comments not available, please download our IOS app");
+		//  comment_buttonx.display = "block";
+		//   comment_buttonx.classList.remove('hidden');
+		createCustomAlert("Comments not available, please download our IOS app");
 
 
 
-	  // if (comment_buttonx.display = "block") {
-   //    comment_buttonx.classList.remove('hidden');
-   //    comment_buttonx.classList.add('block');
-   //  } else {
-   //  	comment_buttonx.classList.add('hidden');
-   //  	comment_buttonx.classList.remove('block');
-   //  }
+		// if (comment_buttonx.display = "block") {
+		//    comment_buttonx.classList.remove('hidden');
+		//    comment_buttonx.classList.add('block');
+		//  } else {
+		//  	comment_buttonx.classList.add('hidden');
+		//  	comment_buttonx.classList.remove('block');
+		//  }
 
 	};
 
@@ -330,11 +358,11 @@ function createPost(body) {
 	div.appendChild(button_div);
 
 	var ALERT_BUTTON_TEXT = "x";
-  if(document.getElementById) {
-    window.alert = function(txt) {
-      createCustomAlert(txt);
-    }
-  }
+	if (document.getElementById) {
+		window.alert = function (txt) {
+			createCustomAlert(txt);
+		}
+	}
 
 	let title = document.createElement('p');
 	let titleNode = document.createTextNode(body.title);

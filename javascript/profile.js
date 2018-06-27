7526e960534a231068359db188c4e5556477bcef
danilaurenhill <dani.lauren.hill@gmail.com>
2018-06-27 12:57:09 -0400

    adds all content and styling only for desktop view

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,4 +1,4 @@
-var divs = ["profile-postsbox", "profile-calendar", "profile-followers", "profile-following"];
+var divs = ["profile-postsbox", "profile-calendar", "profile-followers", "profile-following", "profile-moments"];
 var visibleDivId = null;
 var followers = false;
 var followings = false;

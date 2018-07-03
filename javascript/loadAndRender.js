a55194145b64f4fdcca7b191c3e67d5cce3018f7
Joseph DeWitt <jd@broadwayconnected.com>
2018-07-03 13:02:38 -0400

    favorite via bubble

--- javascript/loadAndRender.js
+++ javascript/loadAndRender.js
@@ -8,9 +8,9 @@ function checkLocalStorage(page) {
 
 	if (PAGE == 'newsfeed') {
 		console.log(PAGE + " page");
-		loadProfile();
-		// loadAndShowPosts();
-		checkIfCompleteProfile();
+		//loadProfile();
+		loadAndShowPosts();
+		//checkIfCompleteProfile();
 
 	} else if (PAGE == 'profile') {
 		console.log(PAGE);
@@ -32,9 +32,9 @@ function loadAndShowPosts() {
 	getPostsItems()
 		.then(newsRaw => {
 			//console.log(newsRaw)
-			newsRaw.forEach(element =>
-				createPost(element)
-			);
+			newsRaw.forEach(element => {
+				createPost(element);
+			});
 		}).then(res => {
 			if (fetchCount > 1) {
 				window.scrollBy({ top: 40, behavior: "smooth" });
@@ -44,28 +44,37 @@ function loadAndShowPosts() {
 }
 
 function getPostsItems() {
-	let url = "http://app.bwayconnected.com/api/";
-	if (PAGE == 'new') {
-		url = url + "posts?offset=" + (12 * fetchCount) + "&limit=12";
+	let url = "https://broadwayconnected.bubbleapps.io/api/1.1/wf/post_read";
+	var body = {
+		'offset': 0,
+		'limit': 100
+	};
+	if (PAGE == 'newsfeed') {
+		body = {
+			'offset': 12*fetchCount,
+			'limit': 12*(fetchCount+1)
+		};
 	}
-	if (PAGE == 'fav') {
-		let user_id = localStorage.getItem("user_id");
-		url = url + "post/favourites?user_id=" + user_id + "&offset=" + (12 * fetchCount) + "&limit=12";
-	}
+	// if (PAGE == 'fav') {
+	// 	let user_id = localStorage.getItem("user_id");
+	// 	url = url + "post/favourites?user_id=" + user_id + "&offset=" + (12 * fetchCount) + "&limit=12";
+	// }
 	fetchCount += 1;
 	//url = url + offset;
 	let params = {
 		headers: {
 			'content-type': 'application/json'
 		},
-		method: 'GET'
+		method: 'POST',
+		body: JSON.stringify(body)
 	}
 
 	return fetch(url, params)
 		.then(res => res.json())
 		.then(body => {
 			wait = false;
-			return body.Result.Posts;
+			console.log(body)
+			return body.response.post;
 		})
 }
 
@@ -89,14 +98,21 @@ function loadProfile() {
 }
 
 
-function favorite(post_id) {
+function favorite(post_id, liked) {
 	//alert("totes fave" + id)
+	console.log(post_id);
 	let user_id = localStorage.getItem("user_id");
-	let url = "http://app.bwayconnected.com/api/post/favourite?user_id=" + user_id + "&post_id=" + post_id;
+	let url = "https://broadwayconnected.bubbleapps.io/api/1.1/wf/favorite"
+	let body = { 
+		'post_id': post_id,
+		'liked': liked
+		}
 	let params = {
 		headers: {
-			'content-type': 'application/json'
+			'content-type': 'application/json',
+			'Authorization': 'Bearer 1530626498553x745134971104562300'
 		},
+		body: JSON.stringify(body),
 		method: 'POST'
 	}
 	console.log(url)
@@ -107,31 +123,33 @@ function favorite(post_id) {
 
 function likesUpdate(post_id) {
 	console.log('update likes post id ', post_id);
-	let url = 'http://app.bwayconnected.com/api/post/detail?post_id=' + post_id;
+	let url = 'https://broadwayconnected.bubbleapps.io/api/1.1/wf/post_read';
 
 	fetch(url, {
-		method: 'GET',
+		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
-		}
+		},
+		body: JSON.stringify({ 'post_id': post_id })
 	})
 		.then(res => res.json())
 		.catch(error => console.error('Error:', error))
-		.then(response => {
+		.then(body => {
 
-			if (response.Response == "1000") {
+			if (body.status != "success") {
 				console.log("request not successful");
 				// createCustomAlert("WARNING: User Already Exists");
 			} else {
-				console.log('Success in likes likesUpdate:', response)
-				var body = response.Result;
-				var likes = body.Posts[0].likes;
+				console.log('Success in likes likesUpdate:', body)
+				var likes = body.response.favoriters;
 				console.log('Likes are', likes)
 
 				let parent = document.getElementById(post_id);
 				console.log('post id in likes', post_id)
 				let fav_likes = parent.querySelector('.fav_likes');
-				fav_likes.innerHTML = likes;
+				console.log(fav_likes);
+				console.log("length ", body.response.post.favoriters.length)
+				fav_likes.innerHTML = body.response.post.favoriters.length;
 
 			}
 		})
@@ -238,7 +256,7 @@ function showModal(body) {
 	let fav_likes = parent.querySelector('.fav_likes');
 
 	fav_button.onclick = function (ev) {
-		favorite(body.id)
+		favorite(body._id)
 			.then(body => {
 				console.log('body', body);
 				if (body.Response == "2000") {
@@ -261,7 +279,7 @@ function showModal(body) {
 }
 
 function createPost(body) {
-	// console.log(body)
+	//console.log(body)
 	let div = document.createElement('div');
 	div.classList.add('rcorners');
 
@@ -270,11 +288,12 @@ function createPost(body) {
 	publisher_div.classList.add('pointer');
 
 	let publisher_image = document.createElement('img');
-	if (body.publisher.profile_image == "http://app.bwayconnected.com/public/images/default.jpg") {
-		publisher_image.src = "http://app.bwayconnected.com/public/images/T3uVwB96tW07.png"
-	} else {
-		publisher_image.src = body.publisher.profile_image;
-	}
+	// if (body.publisher.profile_image == "http://app.bwayconnected.com/public/images/default.jpg") {
+	// 	publisher_image.src = "http://app.bwayconnected.com/public/images/T3uVwB96tW07.png"
+	// } else {
+	// 	publisher_image.src = body.publisher.profile_image;
+	// }
+	
 	publisher_image.classList.add('publisher_image');
 	publisher_div.appendChild(publisher_image);
 
@@ -288,7 +307,7 @@ function createPost(body) {
 	//adding modal .click end
 
 	let publisher_name = document.createElement('p');
-	publisher_nameText = document.createTextNode(body.publisher.first_name + " " + body.publisher.last_name);
+	publisher_nameText = document.createTextNode(body.publisher_name);
 	publisher_name.classList.add('publisher_name');
 
 	publisher_name.appendChild(publisher_nameText);
@@ -309,13 +328,13 @@ function createPost(body) {
 	//adding modal .click end
 
 	let main_image = document.createElement('img');
-	main_image.src = body.post_image;
+	main_image.src = "https://"+ body.image.substr(2);
 	main_image.classList.add('main_image')
 	center1.appendChild(main_image);
 	div.appendChild(center1);
 
 	let button_div = document.createElement('div');
-	button_div.setAttribute("id", body.id);
+	button_div.setAttribute("id", body._id);
 	button_div.classList.add('button_row');
 
 	let user_id = localStorage.getItem("user_id");
@@ -323,28 +342,42 @@ function createPost(body) {
 	// fav_button.setAttribute("id", body.id);
 	fav_button.src = '../images/newsfeed_buttons/heart2.png';
 	fav_button.classList.add('pointer');
-	if (body.favourites.some(fav => fav.user_id == user_id)) {
-		fav_button.classList.add('favorite_click');
-	} else {
-		fav_button.classList.add('button');
+	var liked = false;
+	if(body.favoriters){
+		if (body.favoriters.some(fav => fav == "1530287074040x237806817283853900")) {
+			fav_button.classList.add('favorite_click');
+			liked = true;
+		} else {
+			fav_button.classList.add('button');
+			liked = false;
+		}
 	}
 	fav_button.onclick = function (ev) {
-		favorite(body.id)
+		console.log("like before fetch ", liked)
+		favorite(body._id, liked)
 			.then(body => {
-				console.log('body of Create Post', body);
-				if (body.Response == "2000") {
-					likesUpdate(body.Result.Posts[0].id);
+				if(body.response.post.favoriters.some(fav => fav == "1530287074040x237806817283853900")){
+					var new_like = true;
+				} else {
+					var new_like = false;
 				}
-				if (body.Message === "Added to user favourite successfully") {
+				console.log("new like ", new_like)
+				console.log('body of Create Post', body);
+				if (body.status == "success") {
+					console.log("Success")
+					likesUpdate(body.response.post._id);
+				}
+				if (new_like) {
 					var target = ev.srcElement || ev.target
 					target.classList.remove('button');
 					target.classList.add('favorite_click');
+					liked = true;
 
 				} else {
 					var target = ev.srcElement || ev.target
 					target.classList.add('button');
 					target.classList.remove('favorite_click');
-
+					liked = false;
 				}
 			})
 	};
@@ -353,7 +386,11 @@ function createPost(body) {
 
 
 	let fav_button_num = document.createElement('p');
-	let fav_button_numNode = document.createTextNode(body.likes);
+	if(body.favoriters){
+		var fav_button_numNode = document.createTextNode(body.favoriters.length);
+	} else {
+		var fav_button_numNode = document.createTextNode(0);
+	}
 	fav_button_num.appendChild(fav_button_numNode);
 	fav_button_num.classList.add('fav_likes');
 	button_div.appendChild(fav_button_num);
